11e428d8d377bd3853139ecb6f0cdc651a03f24f
Murphy Stevens <mjstevens@pediatrichomeservice.com>
2019-08-23 12:32:37 -0500

    Added main feed component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainFeedComponent } from './main-feed/main-feed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainFeedComponent
   ],
   imports: [
     BrowserModule,
